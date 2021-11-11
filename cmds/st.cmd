dce74f64943b55d6be8eeebf4ee3c74b4cb1dc4d
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-11-11 14:11:40 +0100

    Cleaned-up the interface.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,4 +2,4 @@
 require ami1700
 
 epicsEnvSet ("STREAM_PROTOCOL_PATH", ".:$(ami1700_DIR)/db")
-iocshLoad("$(ami1700_DIR)/ami1700.iocsh", "ASYN_PORT_NAME=AMI_620,AMI_IP=192.168.1.39")
+iocshLoad("$(ami1700_DIR)/ami1700.iocsh", "ASYN_PORT_NAME=AMI_620,AMI_IP=192.168.1.39,DEV_NAME=LT600,PREFIX=CoW-LHe:")
