739cbbb6a8ed56ddcc76e6b093e20f4808058423
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-11-12 16:36:37 +0100

    Added example opi.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,5 +1,8 @@
 # This should be a test or example startup script
+#require recsync
 require ami1700
-
+epicsEnvSet ("IOCNAME", "nuc-03:ami1700")
 epicsEnvSet ("STREAM_PROTOCOL_PATH", ".:$(ami1700_DIR)/db")
 iocshLoad("$(ami1700_DIR)/ami1700.iocsh", "ASYN_PORT_NAME=AMI_620,AMI_IP=192.168.1.39,DEV_NAME=LT600,PREFIX=CoW-LHe:")
+#iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=nuc-03:ami1700")
+
